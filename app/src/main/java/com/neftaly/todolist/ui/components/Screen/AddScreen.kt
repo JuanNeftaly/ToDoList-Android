5d8b7c98ff54aee9578b4b21f8f9ebb2feae0c35
JuanNeftaly <00182222@uca.edu.sv>
2024-04-30 18:01:04 -0600

    Add: Navigation

--- app/src/main/java/com/neftaly/todolist/ui/components/Screen/AddScreen.kt
+++ app/src/main/java/com/neftaly/todolist/ui/components/Screen/AddScreen.kt
@@ -1,16 +1,36 @@
 package com.neftaly.todolist.ui.components.Screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun AddScreen(){
-    Text(text = "ajhsdkajhs")
+fun AddScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Home", fontSize = 30.sp, color = Color.Blue)
+        }
+    }
 }
 
 @Composable
 @Preview(showSystemUi = true, showBackground = true)
-private fun AddScreenPreview(){
+private fun AddScreenPreview() {
     AddScreen()
 }
